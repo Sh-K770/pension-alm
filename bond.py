506e01becefb4ad59db01896ce2241299d08bee6
Sh-K770 <karimghasemi.sholeh@gmail.com>
2026-07-03 11:09:44 +0200

    Clean repository after package refactor

--- bond.py
+++
@@ -1,9 +0,0 @@
-from asset import Asset
-class Bond(Asset):
-    def __init__(self, id : str, market_value : float, book_value : float, couponRate : float, faceValue : float, maturityYear : int ):
-        super().__init__(id, market_value,book_value)
-        self.couponRate = couponRate
-        self.faceValue = faceValue
-        self.maturityYear = maturityYear
-
-
